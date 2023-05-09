d8f112e896b2d16028ac56c460111e2dc01f67bb
y01and3 <1791280644@qq.com>
2023-05-09 22:34:51 +0800

    clean main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
 pub mod func;
 fn main() {
-    let plain_text =0x0123456789abcdeffedcba9876543210;
-    let sk = 0x0123456789abcdeffedcba9876543210;
-    let enc_text = func::sm4::enc(plain_text, sk);
-    let dec_text = func::sm4::dec(enc_text, sk);
-    println!("plain_text: {:x}", plain_text);
-    println!("enc_text: {:x}", enc_text);
-    println!("dec_text: {:x}", dec_text);
 }

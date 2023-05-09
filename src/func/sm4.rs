3deeb002af06ddf561cdb174edef06dcf0c89044
y01and3 <1791280644@qq.com>
2023-05-09 16:25:37 +0800

    finish enc and dec

---
+++ src/func/sm4.rs
@@ -0,0 +1,23 @@
+use crate::func::{
+    basis_func::{cut_from_128bit, list_invert, merge_to_128bit},
+    f::round,
+    key::key_generator,
+};
+
+pub fn enc(plain_text: u128, sk: u128) -> u128 {
+    let key = key_generator(cut_from_128bit(sk));
+    let mut x = cut_from_128bit(plain_text);
+    for i in 0..32 {
+        x = round(x, key[i]);
+    }
+    merge_to_128bit(list_invert(x))
+}
+
+pub fn dec(enc_text: u128, sk: u128) -> u128 {
+    let key = key_generator(cut_from_128bit(sk));
+    let mut x = cut_from_128bit(enc_text);
+    for i in (0..32).rev() {
+        x = round(x, key[i]);
+    }
+    merge_to_128bit(list_invert(x))
+}
